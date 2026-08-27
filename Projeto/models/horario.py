d219c4e4df4dc7746dbd2d72cfee6e601cf2eb1f
Eduardo23Ribeiro <eduardobr728@gmail.com>
2026-08-27 17:26:14 +0000

    commit4

---
+++ Projeto/models/horario.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+class Horario:
+    def __init__(self, id, data, confirmado, id_cliente, id_servico):
+        self.set_id(id)
+        self.set_data(data)
+        self.set_confirmado(confirmado)
+        self.set_id_cliente(id_cliente)
+        self.set_id_servico(id_servico)
+
+    def set_id(self, id):
+        if not isinstance(id, int):
+            raise TypeError("Id deve ser um número inteiro")
+        if id < 0:
+            raise ValueError("Id deve ser positivo")
+        self.__id = id
+
+    def set_data(self, data):
+        if not isinstance(data, datetime):
+            raise TypeError("Data deve ser do tipo datetime")
+        self.__data = datetime.strptime(data, "%d/%m/%Y %H:%M")
+        
+
+    def set_confirmado(self, confirmado):
+        if not isinstance(confirmado, bool):
+            raise TypeError("Confirmado deve ser True ou False")
+        self.__confirmado = confirmado
+
+    def set_id_cliente(self, id_cliente):
+        if not isinstance(id_cliente, int):
+            raise TypeError("Id do cliente deve ser um número inteiro")
+        if id_cliente < 0:
+            raise ValueError("Id do cliente deve ser positivo")
+        self.__id_cliente = id_cliente
+
+    def set_id_servico(self, id_servico):
+        if not isinstance(id_servico, int):
+            raise TypeError("Id do serviço deve ser um número inteiro")
+        if id_servico < 0:
+            raise ValueError("Id do serviço deve ser positivo")
+        self.__id_servico = id_servico
+
+    def get_id(self):
+        return self.__id
+
+    def get_data(self):
+        return self.__data
+
+    def get_confirmado(self):
+        return self.__confirmado
+
+    def get_id_cliente(self):
+        return self.__id_cliente
+
+    def get_id_servico(self):
+        return self.__id_servico
+
+    def to_json(self):
+        dic = {"id": self.__id, "data":self.__data.strftime("%d/%m/%Y %H:%M"), \
+               "confirmado": self.__confirmado, "id_cliente":self.__id_cliente,
+               "id_servico":self.__id_servico
+
+        }
+    @staticmethod
+    def from_json(dic):
+        horario = Horario(dic["id"], datetime.strptime())  
---
+++ Projeto/templates/manterhorarioui.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+import time
+from service import Service
+
+
+
+
+
+class ManterHorarioUI:
+    def main():
+        st.header("Cadrastro de Serviço")
+        tab1, tab2, tab3, tab4 = st.tabs(
+            ["Inserir", "Listar", "Atualizar", "Excluir"]
+        )
+        with tab1: ManterHorarioUI.servico_inserir()
+        with tab2: ManterHorarioUI.servico_listar()
+        with tab3: ManterHorarioUI.servico_atualizar()
+        with tab4: ManterHorarioUI.servico_excluir()
+
+    @staticmethod
+    def servico_listar():
+        servico = Service.servico_listar()
+        if not servico:
+            st.write("Nenhum serviço cadastrado.")
+            return
+
+        df = pd.DataFrame([servico.to_json() for servico in servico])
+        st.dataframe(df, hide_index=True)
+
+    @staticmethod
+    def servico_inserir():
+        st.header("Cadastro de Serviços")
+        id = st.number_input("Informe o id:", min_value=0, step=1)
+        nome = st.text_input("Informe o nome:")
+        email = st.text_input("Informe o e-mail:")
+        fone = st.text_input("Informe o telefone:")
+        if st.button("Inserir"):
+            Service.servico_inserir(id, nome, email, fone)
+            st.success("Serviço inserido com sucesso!")
+            st.write(f"Serviço inserido: {nome}")
+
+    @staticmethod
+    def servico_atualizar():
+        servicos = Service.servico_listar()
+        if not servicos:
+            st.write("Nenhum Serviço cadastrado.")
+            return
+
+        servico = st.selectbox(
+            "Atualização de Serviço",
+            servicos,
+            format_func=str,
+        )
+        nome = st.text_input("Novo nome", value=servico.get_nome())
+        email = st.text_input("Novo e-mail", value=servico.get_email())
+        fone = st.text_input("Novo fone", value=servico.get_fone())
+        if st.button("Atualizar"):
+            Service.cliente_atualizar(servico.get_id(), nome, email, fone)
+            st.success("Serviço atualizado com sucesso!")
+
+    @staticmethod
+    def servico_excluir():
+        servicos = Service.servico_listar()
+        if not servicos:
+            st.write("Nenhum cliente cadastrado.")
+            return
+
+        servico = st.selectbox(
+            "Exclusão de Serviços",
+            servicos,
+            format_func=str,
+        )
+        if st.button("Excluir"):
+            Service.servico_excluir(servico.get_id())
+            st.success("Serviço excluído com sucesso!")
